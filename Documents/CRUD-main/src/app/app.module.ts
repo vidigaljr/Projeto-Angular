93a4f44b6c7339a01e68d36e673107bd49105d19
vidigaljr <ivan.vidigaljr@gmail.com>
2022-09-13 06:46:04 -0300

    CRUDANGULAR

---
+++ Documents/CRUD-main/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';{}
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { AlunosService } from './alunos.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlunosComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+
+  ],
+  providers: [AlunosService, HttpClientModule],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
